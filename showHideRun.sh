e9a91db514fd60842dfafcebd84b48c90ae3a348
Administrator <admin@local.host>
2014-12-28 13:19:14 +0400

    Script for search window by classname: if found - active, else - run given programm

---
+++ showHideRun.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+( ! which xdotool &>/dev/null || ! which wmctrl &>/dev/null ) \
+    && echo "No xdotool or wmctrl found in system" && exit 1
+
+CLASS="$1"
+RUN="$2"
+
+[[ -z "$CLASS" || -z "$RUN" ]] \
+    && echo "Usage: "$0" [class-name] [command-to-run]" && exit 0
+
+WINRAWID=`wmctrl -x -l | grep ${CLASS} | awk '{print $1}' | head -n 1`
+WINID=$((${WINRAWID}))
+
+if [ -z "${WINRAWID}" ]; then
+    ${RUN}
+else
+    ACTIVEWIN=`xdotool getactivewindow`
+    if [ "${ACTIVEWIN}" == "${WINID}" ]; then
+        xdotool windowminimize ${WINID}
+    else
+        xdotool windowactivate ${WINID}
+    fi
+fi
